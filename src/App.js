642ddf46879f692483a0655ec2dda7f3b94f0e9a
Tania Olivia <tania.olivia69@yahoo.com>
2024-06-19 17:35:53 +0200

    add page contact, concept and menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Concept from './components/Concept';
+import Contact from './components/Contact';
+import Menu from './components/Menu';
+import MenuCarousel from './components/MenuCarousel';
 import './sass/App.scss';
 
 function App() {
   return (
     <div className="app">
       <Concept></Concept>
+      <Menu></Menu>
+      <MenuCarousel></MenuCarousel>
+      <Contact></Contact>
     </div>
   );
 }
--- src/components/Concept.js
+++ src/components/Concept.js
@@ -1,7 +1,39 @@
 function Concept() {
   return (
     <div className="concept">
-        <img src="../images/concept.jpg" alt=""/>
+        <img src="../images/restaurant.png" alt="" className="concept--image"/>
+
+        <div className="concept--text">
+          <h1 className="concept--title">Le concept : La cuisine italienne rapide</h1>
+
+          <p className="concept--description">
+            Pasta e Polpette propose un concept innovant qui combine l'authenticité des recettes traditionnelles italiennes, avec la rapidité et la commodité des fast foods modernes. Nous vous offrons des plats de qualité, préparés avec des ingrédients frais et authentiques... en moins de 5 minutes.
+          </p>
+
+          <div className="concept--skills">
+              <div className="concept--skill">
+                  <img src="../images/authentique.png" alt="Authentique" className="concept--icon"/>
+                  <div className="text">Authentique</div>
+              </div>
+
+              <div className="concept--skill">
+                  <img src="../images/rapide.png" alt="Authentique" className="concept--icon"/>
+                  <div className="text">Rapide</div>
+              </div>
+          </div>
+
+          <div className="concept--skills">
+              <div className="concept--skill">
+                  <img src="../images/qualitatif.png" alt="Authentique" className="concept--icon"/>
+                  <div className="text">Qualitatif</div>
+              </div>
+
+              <div className="concept--skill command">
+                  <img src="../images/commande-en-ligne.png" alt="Authentique" className="concept--icon"/>
+                  <div className="text">Commande en ligne et sur place</div>
+              </div>
+          </div>
+        </div>
     </div>
   );
 }
